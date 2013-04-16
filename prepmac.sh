b5e5455bb7f32233819a4690dc70d1d86c3802b7
Nick Van den Bleeken <nick@bleeken.eu>
2013-04-16 10:08:50 +0200

    Using extrenal boost on mac

--- prepmac.sh
+++ prepmac.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./firebreath/prepmac.sh . build
+./firebreath/prepmac.sh . build -D BOOST_ROOT=/usr/local/include/boost -DWITH_SYSTEM_BOOST=1 -D Boost_USE_STATIC_LIBS=on -D Boost_USE_STATIC_RUNTIME=on
