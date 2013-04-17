dde025675b56e07f7d204df6fbb6da124c92c351
nhofsted <nick.hofstede@inventivegroup.com>
2013-04-17 12:13:30 +0200

    Added system boost library to windows setup

--- prep2012x64.cmd
+++ prep2012x64.cmd
@@ -1,1 +1,1 @@
-call .\firebreath\prep2012x64.cmd . build
+call .\firebreath\prep2012x64.cmd . build "-DBOOST_ROOT=C:\Program Files\boost" "-DWITH_SYSTEM_BOOST=1" "-DBoost_USE_STATIC_LIBS=on" "-DBoost_USE_STATIC_RUNTIME=on"
